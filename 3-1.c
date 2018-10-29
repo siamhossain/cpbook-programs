15579df4339638d250c26ca661553c248779ad23
Siam Hossain <siam.hossain1994@gmail.com>
2018-10-29 13:38:20 +0600

    Solve problem 3.1

---
+++ 3-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number : ");
+    scanf("%d", &a);
+    if( a >= 0 ){
+        printf("The number is positive\n");
+    }
+    else{
+        printf("The number is negetive");
+    }
+
+
+}

162b1b2a2672179f9839b966886b1cbf7d882ee2
Siam Hossain <siam.hossain1994@gmail.com>
2018-10-29 13:34:56 +0600

    Solve problem 2.13

---
+++ 2-13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch;
+
+    printf("Enter the first letter of your name : ");
+    ch = getchar();
+    printf("The 1st letter of your name is : %c",ch);
+    return 0;
+
+}

034e865fc493cbbab553d96a2a50eca78dec277d
Siam Hossain <siam.hossain1994@gmail.com>
2018-10-29 13:28:07 +0600

    Solve problem 2-9.c

---
+++ 2-9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    double a, b, sum;
+
+    a = 9.5;
+    b = 8.743;
+
+    sum = a+b;
+
+    printf("Ther sum is %lf\n",sum);
+
+    printf("Ther sum is %0.0lf",sum);
+
+return 0;
+
+}

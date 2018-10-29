ad25783655cd3da2eb89d9adbb8e831babd2ec96
Siam Hossain <siam.hossain1994@gmail.com>
2018-10-29 14:37:30 +0600

    Solve problem 5-8.c

---
+++ 5-8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int a, b, x, gcd;
+    printf("Enter tow numbers : ");
+    scanf("%d %d", &a, &b);
+
+    if ( a < b ){
+        x = a;
+    }
+    else{
+        x = b;
+    }
+
+    for ( ; x >= 1; x--){
+        if( a % x == 0 && b % x == 0){
+            gcd = x;
+            break;
+        }
+    }
+    printf("GCD is %d", gcd);
+    return 0;
+}
